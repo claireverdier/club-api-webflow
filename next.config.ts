a594e0479c69166a374a45b6397e8250b6c54087
Claire Verdier <claire.verdier@orkester.fr>
2025-11-10 18:21:10 +0100

    remove eslint

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   // @ts-expect-error – Webflow Cloud dynamic basePath
-  basePath: mountPath === "/" ? "" : mountPath,
   assetPrefix:
     "https://4673958c-f79f-4c4d-bc6b-da3d64cfe2fb.wf-app-prod.cosmic.webflow.services/app",
   images: {
