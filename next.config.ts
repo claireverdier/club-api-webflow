4805f75e968b791ecf7d459f2ebfcb4dbd9d72c9
Claire Verdier <claire.verdier@orkester.fr>
2025-11-10 18:26:41 +0100

    remove ignore error

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // @ts-expect-error – Webflow Cloud dynamic basePath
   assetPrefix:
     "https://4673958c-f79f-4c4d-bc6b-da3d64cfe2fb.wf-app-prod.cosmic.webflow.services/app",
   images: {
