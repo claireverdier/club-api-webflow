440a47c1716809f5020a7c77c6aef144ab6dd030
claireverdier <claire.verdier@orkester.fr>
2025-11-10 15:21:13 +0100

    Update clubs.js

--- api/clubs.js
+++ api/clubs.js
@@ -1,5 +1,5 @@
 export async function GET(request) {
-    return new Response(JSON.stringify({ message: "Hello depuis Webflow Cloud!" }), {
+    return new Response(JSON.stringify({ message: "Hello2 depuis Webflow Cloud!" }), {
         headers: { "Content-Type": "application/json" }
     });
 }
