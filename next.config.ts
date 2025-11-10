4f464465b2113022d7f0e1d60ef1db7237c1f799
Claire Verdier <claire.verdier@orkester.fr>
2025-11-10 18:09:16 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
+const mountPath = process.env.WEBFLOW_MOUNT_PATH || "/app";
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  basePath: "/app",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // @ts-expect-error – Webflow Cloud dynamic basePath
+  basePath: mountPath === "/" ? "" : mountPath,
+  assetPrefix:
+    "https://4673958c-f79f-4c4d-bc6b-da3d64cfe2fb.wf-app-prod.cosmic.webflow.services/app",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
-// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
