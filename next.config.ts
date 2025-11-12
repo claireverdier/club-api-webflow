59065e53822112eccbdb967233110284b5670b13
claireverdier <claire.verdier@orkester.fr>
2025-11-12 09:43:17 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
-const mountPath: string = process.env.WEBFLOW_MOUNT_PATH || "/app";
+// const mountPath: string = process.env.WEBFLOW_MOUNT_PATH || "/app";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  basePath: mountPath === "/" ? "" : mountPath,
+  basePath: "/app",
   assetPrefix:
     "https://4673958c-f79f-4c4d-bc6b-da3d64cfe2fb.wf-app-prod.cosmic.webflow.services/app",
   images: {
